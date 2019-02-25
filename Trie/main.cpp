37a7881dca066d1b07eca8976893a1d7ef1b4ff6
Ankur Sao <ankur.sao7@gmail.com>
2019-02-25 20:38:06 +0530

    Initial Commit

---
+++ Trie/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Trie
+//
+//  Created by ansao on 2/25/19.
+//  Copyright © 2019 ansao. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
